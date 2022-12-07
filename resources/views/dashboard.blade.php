2ebefe0dc80c78d87a547160c841f0d97f58520f
Arturo Quirós <joseq2408@gmail.com>
2022-12-06 19:47:24 -0600

    nombre agregado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h1>JOSE ARTURO QUIROS ARAYA</h1>
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
